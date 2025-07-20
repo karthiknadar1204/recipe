b8efa0bdfb024c27e6903964e7fcb0e4e9baf978
karthik <karthiknadar1204@gmail.com>
2025-07-20 10:45:07 +0530

    basic server setup

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import { ENV } from "./config/env.js";
+
+const app=express()
+const PORT=ENV.PORT
+
+
+app.use(express.json())
+
+
+app.get("/api/health", (req, res) => {
+    res.status(200).json({ success: true });
+  });
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
